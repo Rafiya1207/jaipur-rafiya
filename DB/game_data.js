6020d08743adabc2864bfd2d720406495efafd5a
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-12-16 17:04:01 +0530

    remove makeDeck and gamesMap

--- DB/game_data.js
+++ DB/game_data.js
@@ -11,32 +11,61 @@ l - leather
 
  */
 
-export const makeDeck = (goodsMap) => {
-  const deck = [];
-
-  for (const good in goodsMap) {
-    deck.push(goodsMap[good].alias.repeat(goodsMap[good].count).split(""));
-  }
-  return deck.flatMap((x) => x);
-};
-
-const goodsMap = {
-  ruby: { symbol: "♦️ ", count: 6, alias: "r", coins: [7, 7, 5, 5, 5] },
-  gold: { symbol: "⚱️ ", count: 6, alias: "g", coins: [6, 6, 5, 5, 5] },
-  silver: { symbol: "🪙 ", count: 6, alias: "v", coins: [5, 5, 5, 5, 5] },
-  cloth: { symbol: "👕 ", count: 8, alias: "o", coins: [5, 3, 3, 2, 2, 1, 1] },
-  spices: { symbol: "🌶️ ", count: 8, alias: "p", coins: [5, 3, 3, 2, 2, 1, 1] },
-  leather: {
-    symbol: "💼 ",
-    count: 10,
-    alias: "l",
-    coins: [4, 3, 2, 1, 1, 1, 1, 1, 1],
-  },
-  camels: { symbol: "🐪 ", count: 8, alias: "m", coins: [5] },
-};
-
 export const gameData = {
-  deck: makeDeck(goodsMap),
+  deck: [
+    "r",
+    "r",
+    "r",
+    "r",
+    "r",
+    "r",
+    "g",
+    "g",
+    "g",
+    "g",
+    "g",
+    "g",
+    "v",
+    "v",
+    "v",
+    "v",
+    "v",
+    "v",
+    "o",
+    "o",
+    "o",
+    "o",
+    "o",
+    "o",
+    "o",
+    "o",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "p",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "l",
+    "m",
+    "m",
+    "m",
+    "m",
+    "m",
+    "m",
+    "m",
+    "m",
+  ],
   market: ["m", "m", "m"],
   goodsMap: {
     ruby: { symbol: "♦️ ", count: 6, alias: "r", coins: [7, 7, 5, 5, 5] },
@@ -72,3 +101,5 @@ export const gameData = {
     { playerId: 2, playerName: "", herd: [], hand: [] },
   ],
 };
+
+console.log(gameData);
